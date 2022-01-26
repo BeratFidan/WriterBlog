86500d88e9133aeb39b200cba2802c834341ba4f
Berat <Berat@DESKTOP-CC2M814>
2022-01-26 17:02:11 +0300

    Katmanlar oluşturuldu

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBL(Category parcategory)
+        {
+            if (parcategory.CategoryName == "" || parcategory.CategoryName.Length <= 3 || parcategory.CategoryDescription == "" || parcategory.CategoryName.Length >= 51)
+            {
+                //hata mesajı
+            }
+            else
+            {
+                repo.Insert(parcategory);
+            }
+        }
+    }
+}
